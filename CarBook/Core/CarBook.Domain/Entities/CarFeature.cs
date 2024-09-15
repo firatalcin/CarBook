ac7b495850c83f5f905a027d43c55801ae1c59f7
Fırat Alçın <firatalcin04@gmail.com>
2024-09-15 20:07:26 +0300

    Entities

---
+++ CarBook/Core/CarBook.Domain/Entities/CarFeature.cs
@@ -0,0 +1,12 @@
+﻿namespace CarBook.Domain.Entities
+{
+    public class CarFeature
+    {
+        public int Id { get; set; }
+        public int CarId { get; set; }
+        public Car Car { get; set; }
+        public int FeatureId { get; set; }
+        public Feature Feature { get; set; }
+        public bool Available { get; set; }
+    }
+}
---
+++ CarBook/Core/CarBook.Domain/Entities/Location.cs
@@ -0,0 +1,11 @@
+﻿namespace CarBook.Domain.Entities
+{
+    public class Location
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<RentACar> RentACars { get; set; }
+        public List<Reservation> PickUpReservation { get; set; }
+        public List<Reservation> DropOffReservation { get; set; }
+    }
+}
