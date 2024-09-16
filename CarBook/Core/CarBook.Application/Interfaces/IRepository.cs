e1277bcae9b1a2fcaeaf23a42333e4296c43a607
Fırat Alçın <firatalcin04@gmail.com>
2024-09-16 23:45:46 +0300

    IRepository Interface

---
+++ CarBook/Core/CarBook.Application/Interfaces/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace CarBook.Application.Interfaces
+{
+    internal interface IRepository<T> where T : class
+    {
+        Task<List<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task CreateAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task RemoveAsync(int id);
+    }
+}
