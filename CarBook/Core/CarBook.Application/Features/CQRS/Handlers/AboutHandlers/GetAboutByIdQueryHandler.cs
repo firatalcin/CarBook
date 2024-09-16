245dde3f2f72f5d833d7c13b4e0164c037c3d927
Fırat Alçın <firatalcin04@gmail.com>
2024-09-17 00:11:40 +0300

    Get About By Id Query Handler Sınıfı

--- CarBook/Core/CarBook.Application/Features/CQRS/Handlers/AboutHandlers/GetAboutByIdQueryHandler.cs
+++ CarBook/Core/CarBook.Application/Features/CQRS/Handlers/AboutHandlers/GetAboutByIdQueryHandler.cs
@@ -1,6 +1,29 @@
-﻿namespace CarBook.Application.Features.CQRS.Handlers.AboutHandlers
+﻿using CarBook.Application.Features.CQRS.Queries.AboutQueries;
+using CarBook.Application.Features.CQRS.Results.AboutResults;
+using CarBook.Application.Interfaces;
+using CarBook.Domain.Entities;
+
+namespace CarBook.Application.Features.CQRS.Handlers.AboutHandlers
 {
-    internal class GetAboutByIdQueryHandler
+    public class GetAboutByIdQueryHandler
     {
+        private readonly IRepository<About> _repository;
+
+        public GetAboutByIdQueryHandler(IRepository<About> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<GetAboutByIdQueryResult> Handle(GetAboutByIdQuery request)
+        {
+            var about = await _repository.GetByIdAsync(request.Id);
+            return new GetAboutByIdQueryResult
+            {
+                Id = about.Id,
+                Description = about.Description,
+                ImageUrl = about.ImageUrl,
+                Title = about.Title
+            };
+        }
     }
 }
