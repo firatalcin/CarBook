64a94b443335cf6fd91c0ddc93049ea44c45ec77
Fırat Alçın <firatalcin04@gmail.com>
2024-09-17 23:48:33 +0300

    About Command Handler Yazılması

---
+++ CarBook/Core/CarBook.Application/Features/CQRS/Handlers/AboutHandlers/CreateAboutCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using CarBook.Application.Features.CQRS.Commands.AboutCommands;
+using CarBook.Application.Interfaces;
+using CarBook.Domain.Entities;
+
+namespace CarBook.Application.Features.CQRS.Handlers.AboutHandlers
+{
+    public class CreateAboutCommandHandler
+    {
+        private readonly IRepository<About> _repository;
+
+        public CreateAboutCommandHandler(IRepository<About> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(CreateAboutCommand request)
+        {
+            var about = new About 
+            {
+                Title = request.Title,
+                Description = request.Description,
+                ImageUrl = request.ImageUrl
+            };
+
+            await _repository.CreateAsync(about);
+
+        }
+    }
+}
