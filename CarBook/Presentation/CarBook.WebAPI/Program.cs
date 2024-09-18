71fb3874397244d37beb97a7be18f7d8f53ecd9c
Fırat Alçın <firatalcin04@gmail.com>
2024-09-18 23:38:30 +0300

    Apinin Test Edilmesi

--- CarBook/Presentation/CarBook.WebAPI/Program.cs
+++ CarBook/Presentation/CarBook.WebAPI/Program.cs
@@ -1,4 +1,9 @@
 
+using CarBook.Application.Features.CQRS.Handlers.AboutHandlers;
+using CarBook.Application.Interfaces;
+using CarBook.Persistence.Context;
+using CarBook.Persistence.Repositories;
+
 namespace CarBook.WebAPI
 {
     public class Program
@@ -8,6 +13,13 @@ namespace CarBook.WebAPI
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddScoped<AppDbContext>();
+            builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+            builder.Services.AddScoped<GetAboutQueryHandler>();
+            builder.Services.AddScoped<GetAboutByIdQueryHandler>();
+            builder.Services.AddScoped<CreateAboutCommandHandler>();
+            builder.Services.AddScoped<UpdateAboutCommandHandler>();
+            builder.Services.AddScoped<RemoveAboutCommandHandler>();
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
